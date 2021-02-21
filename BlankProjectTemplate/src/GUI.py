10bb96cdd3c636d620b04720611cf5a3afb3a075
Dylan Smith <smithd35@mcmaster.ca>
2021-02-21 15:00:36 -0500

    GUI displaying boardstate

--- BlankProjectTemplate/src/GUI.py
+++ BlankProjectTemplate/src/GUI.py
@@ -3,3 +3,81 @@ from board import *
 
 class GUI:
     def __init__(self):
+        #import images
+        self.board_img = pygame.image.load('./img/board.png')
+        self.red_piece = pygame.image.load('./img/red_man.png')
+        self.white_piece = pygame.image.load('./img/white_man.png')
+        #self.start_button = pygame.image.load('./img/start_button.png')
+        #self.tutorial_button = pygame.image.load('./img/tutorial_button.png')
+        #get dimensions of board
+        self.board_height = self.board_img.get_height()
+        self.board_width = self.board_img.get_width()
+        self.num_cols = 8
+        self.num_rows = 8
+        self.screen = None
+        self.make_display()
+
+    def make_display(self):
+        self.screen = pygame.display.set_mode((800, 800))
+        pygame.display.set_caption('Checkers')
+
+    def display_board(self, board_state):
+        self.screen.fill((200,0,0))
+        self.screen.blit(self.board_img, (0, 0))
+        #Adding pieces
+        y = 0
+        for col in board_state:
+            x = 0
+            for row in col:
+                self.display_piece(row,x,y)
+                x+=1
+            y+=1
+        pygame.display.update()
+
+    def display_message(message):
+        pass
+
+    def display_piece(self,colour, row, col):
+        if colour == 'R':
+            self.screen.blit(self.red_piece, self.calc_pos(row, col))
+        elif colour == 'W':
+            self.screen.blit(self.white_piece, self.calc_pos(row, col))
+        else:
+            pass
+
+
+    def calc_pos(self, row, col):
+        x = row * self.board_width/self.num_cols
+        y = col * self.board_height/self.num_rows
+        return (x,y)
+
+#testing
+# b = [['','W','','W','','W','','W'],\
+#                 ['W','','W','','W','','W',''],\
+#                 ['','W','','W','','W','','W'],\
+#                 ['','','','','','','',''],\
+#                 ['','','','','','','',''],\
+#                 ['R','','R','','R','','R',''],\
+#                 ['','R','','R','','R','','R'],\
+#                 ['R','','R','','R','','R','']]
+#
+# gui = GUI()
+#
+# run = True
+# clock = pygame.time.Clock()
+# while run:
+#     #slows down the while loop to make it run the same speed on diff computers
+#     clock.tick(60)
+#     pass
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             run = False
+#         if event.type == pygame.MOUSEBUTTONDOWN:
+#             pass
+#         else:
+#             pass
+#     gui.display_board(b)
+#
+# #closes screen when the loop ends
+# #loop ends when we the "x" on the screen is clicked
+# pygame.quit()
