f9ceef43d3d86d3f05e48aa1d0cbdacb2a8d7357
Dylan Smith <smithd35@mcmaster.ca>
2021-03-22 19:43:36 -0400

    added end of game logic

--- BlankProjectTemplate/src/game.py
+++ BlankProjectTemplate/src/game.py
@@ -7,6 +7,7 @@ class Game():
         self.selected = None
         self.turn = "RED"
         self.validMoves = {}
+        self.winner = ""
 
 
     def select(self, square):
@@ -27,6 +28,10 @@ class Game():
                 self.board.move(self.selected,row,col,self.validMoves[(row,col)])
                 self.selected = None
                 self.validMoves = {}
+                print(self.turn)
+                self.checkGameEnd()
+                self.changeTurn()
+                print(self.turn)
             else:
                 print("Can't move there")
                 self.selected = None
@@ -36,6 +41,29 @@ class Game():
 
     def changeTurn(self):
         if self.turn == "RED":
-            self.turn == "WHITE"
+            self.turn = "WHITE"
         else:
-            self.turn == "RED"
+            self.turn = "RED"
+
+    def checkGameEnd(self):
+        moves = {}
+        if self.turn == "RED":
+            for piece in self.board.red_pieces:
+                moves.update(self.board.getValidMoves(piece))
+                #if red has any valid moves then white has not won
+                if moves:
+                    break
+            #if moves is empty then white wins
+            if not moves:
+                self.winner = "WHITE"
+                print("white Wins!")
+        else:
+            for piece in self.board.white_pieces:
+                moves.update(self.board.getValidMoves(piece))
+                #if white has any valid moves then red has not won
+                if moves:
+                    break
+            #if moves is empty then red wins
+            if not moves:
+                self.winner = "RED"
+                print("Red Wins!")
