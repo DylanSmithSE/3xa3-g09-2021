36ee9ef236b54b7e16242c6d8e7c63571957f235
Dylan Smith <smithd35@mcmaster.ca>
2021-04-05 11:35:00 -0400

    documenting the board class

--- BlankProjectTemplate/src/board2.py
+++ BlankProjectTemplate/src/board2.py
@@ -1,22 +1,34 @@
 ## @file board.py
-#  @author Carson Wilcox, Ardhendu, Dylan, Thaneegan
-#  @brief Class to represent the board and handle the state of the board of the checkers game.
-#  @date March 16th 2021
-
+#  @title Board
+#  @details Reference: https://github.com/techwithtim/Python-Checkers-AI
+#  @author Ardhendu, Dylan, Thaneegan
+#  @date April 5th 2021
 from pieces2 import *
 from constants import *
 
+## @brief The Board class handles the state of the board. Its main function is to
+#   determine where pieces of the board can move.
 class Board():
+    ## @brief The constructor initializes the member variables and calls setBoard
+    #  @param gui The GUI is passed in to relay information stored withing the
+    #   GUI class to the setBoard method
     def __init__(self, gui, board = None, turn = "RED"):
+        ## turn stores the piece colour that can move
         self.turn = turn
         self.gameWon = -1
+        ## boardState stores the state of the board.
         self.boardState = [[0] * ROWS for x in range(COLS)]
+        ## red_pieces stores all the red pieces that remain
         self.red_pieces = []
+        ## white_pieces stores all the white pieces that remain
         self.white_pieces = []
+        ## stores the colour of the winner when the game ends
         self.winner = ""
         self.setBoard(gui, board)
 
-
+    ## @brief resetBoard resets the member variables and calls setBoard
+    #  @param gui The GUI is passed in to relay information stored withing the
+    #   GUI class to the setBoard method
     def resetBoard(self, gui):
         self.turn = "RED"
         self.gameWon = -1
@@ -28,6 +40,10 @@ class Board():
         # Tk().wm_withdraw() #to hide the main window
         # messagebox.showinfo('Starting new game...','New game selected, enjoy!')
 
+    ## @brief setBoard sets the board state based on the colour of pieces the
+    #   user chooses
+    #  @param gui The GUI is passed in to relay the piece colour choice made by
+    #   the user
     def setBoard(self, gui, board = None):
         #overloading the constructor to help with testing
         if board:
@@ -67,7 +83,13 @@ class Board():
                 y = y + 1
             x = x + 1
 
-    #replace piece with 0 and then moves the piece to the new location
+    ## @brief The move method takes a piece and moves it to the given location
+    #  @details It must move the piece to the specified location and remove all
+    #   all the pieces that are jumped along the way
+    #  @param piece Is the piece that will be moving
+    #  @param move (row,col) Is the location the piece will be moving to
+    #  @param skipped [(row,col),(row,col),...] Is an array of the locations
+    #   of the pieces that are jumped and need to be removed
     def move(self, piece, move, skipped):
         self.boardState[piece.row][piece.col] = 0
         self.boardState[move[0]][move[1]] = piece
@@ -83,6 +105,8 @@ class Board():
             self.remove(self.boardState[p[0]][p[1]])
         self.changeTurn()
 
+    ## @brief The remove method takes a piece and removes it from the board
+    #  @param piece The piece being removed
     def remove(self, piece):
         self.boardState[piece.row][piece.col] = 0
         if piece in self.red_pieces:
@@ -90,12 +114,18 @@ class Board():
         if piece in self.white_pieces:
             self.white_pieces.remove(piece)
 
+    ## @brief Changes the turn
     def changeTurn(self):
         if self.turn == "RED":
             self.turn = "WHITE"
         else:
             self.turn = "RED"
 
+    ## @brief Checks to see if the boardState is in a win/loss state
+    #  @details Checks to see if the player with the current turn has any moves
+    #   remaining.
+    #  @return True when the boardState is in a win/loss state
+    #  @return False when the boardState is not in a win/loss state
     def checkGameEnd(self):
         moves = {}
         if self.turn == "RED":
@@ -125,15 +155,27 @@ class Board():
             else:
                 return False
 
+    ## @brief Determines the score of a position. Is used by the minmax function
+    #  @details Each player will try to maximize the difference in the number
+    #   of pieces they have vs the number of pieces their opponent has
     def evaluateBoard(self):
         return len(self.white_pieces) - len(self.red_pieces)
 
+    ## @brief Given a colour, returns all the pieces of that colour
+    #  @param colour The colour of pieces to be returned
+    #  @return Returns the array of pieces, of the specified colour
     def getPieces(self, colour):
         if colour == "RED":
             return self.red_pieces
         else:
             return self.white_pieces
 
+    ## @brief Returns all of the valid moves that a piece can make
+    #  @param piece The piece to determine the valid moves for
+    #  @return moves {(row,col): [(row,col),...],...} A dictionary where the keys
+    #   are the location (stored as a tuple) of all the valid moves the piece can make,
+    #   the corresponding value is an array of locations (stored as tuples)
+    #   representing the pieces that are jumped in making that move.
     def getValidMoves(self, piece):
         moves = {}
         row = piece.row
