9584e4806f5b44dcea9032ec5e466cf9728927ee
Dylan Smith <smithd35@mcmaster.ca>
2021-04-06 00:19:08 -0400

    Added comments for minmax2

--- BlankProjectTemplate/src/minmax2.py
+++ BlankProjectTemplate/src/minmax2.py
@@ -1,9 +1,22 @@
-#reference: tech with tim (https://github.com/techwithtim/Python-Checkers-AI)
+## @file minmax2.py
+#  @details Reference: https://github.com/techwithtim/Python-Checkers-AI
+#  @author Ardhendu, Dylan, Thaneegan
+#  @date April 5th 2021
 from copy import deepcopy
 from board2 import *
 import pygame
 
-#takes in the board to consider, a boolean: true for maxplayer false for min player, recursion depth
+## @brief Minmax determines the best move that a player can make
+#  @param currentBoard Is a board object representing the state of the game to
+#   be evaluated
+#  @param maxPlayer is a boolean value. It is true when the player calling minmax
+#   is the player maximizing the score (white pieces in our case). It is negative
+#   when the player calling minmax is minimizing the score.
+#  @param depth Is the recursive depth that the algorithm will search. It is an
+#   exponential algorithm so the higher the recursive depth the slower the AI
+#   performs.
+#  @return bestBoard.evaluateBoard(), bestBoard The minmax function returns the
+#   score of the best move, as well as the board after the best move has been made. 
 def minmax(currentBoard,maxPlayer,depth):
     #if the we are at the furthest depth or the game is over we can return the boardScore
     if depth == 0 or currentBoard.checkGameEnd():
@@ -14,7 +27,7 @@ def minmax(currentBoard,maxPlayer,depth):
         maxScore = float('-inf')
         bestBoard = None
         #go through all possible moves get evaluation of the boards
-        for board in getAllBoards(currentBoard):
+        for board in _getAllBoards(currentBoard):
             score = minmax(board,False,depth-1)[0]
             maxScore = max(maxScore,score)
             if maxScore == score:
@@ -24,7 +37,7 @@ def minmax(currentBoard,maxPlayer,depth):
         minScore = float('inf')
         bestBoard = None
         #go through all possible moves get evaluation of the boards
-        for board in getAllBoards(currentBoard):
+        for board in _getAllBoards(currentBoard):
             score = minmax(board,True,depth-1)[0]
             minScore = min(minScore,score)
             if minScore == score:
@@ -32,7 +45,7 @@ def minmax(currentBoard,maxPlayer,depth):
         return bestBoard.evaluateBoard(), bestBoard
 
 #gets a board, based on the current turn. simulates all possible moves and returns a list of boards
-def getAllBoards(currentBoard):
+def _getAllBoards(currentBoard):
     newBoards = []
     #for every piece on the board that can move
     for piece in currentBoard.getPieces(currentBoard.turn):
